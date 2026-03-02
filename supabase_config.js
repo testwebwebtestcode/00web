4a246dc7949dee6455d000d11fbd7cc9351cb707
testwebwebtestcode <sumaya.bader1@gmail.com>
2026-03-02 16:25:50 +0300

    Add files via upload

--- supabase_config.js
+++ supabase_config.js
@@ -6,9 +6,9 @@
 ========================================================= */
 
 window.SUPABASE_URL = "https://pwpriirdjhfaxtcjxmee.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_d43HFAfSUZJdI2yXPxPzlA_cPyXChFe";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3cHJpaXJkamhmYXh0Y2p4bWVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3MDUyMDgsImV4cCI6MjA4NTI4MTIwOH0.M2btwcTtWyTNgY2kCgiUEhv7yXIcEN7rWuLkzzDC8Pk";
 
-window.supabaseClient = supabase.createClient(
+window.sb = supabase.createClient(
   window.SUPABASE_URL,
   window.SUPABASE_ANON_KEY
 );
